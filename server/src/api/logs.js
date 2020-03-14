c06bd172ec045f6fd52a51597500591891de0193
yeounseo <yeoandante@gmail.com>
2020-03-14 11:22:14 +0900

    modified logs.js index.js files

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -1,29 +1,31 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
-const LogEntry = require('../models/LogEntry');
+const LogEntry = require("../models/LogEntry");
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        message: '🌍',
-    });
+router.get("/", async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/', async (req, res, next) => {
-    try {
-        const logEntry = new LogEntry(req.body);
-        const createdEntry = await logEntry.save();
-        res.json(createdEntry);
-    } catch (error) {
-        // console.log(error.name);
-        if (error.name === 'ValidationError') {
-            res.status(422);
-        }
-        next(error);
+router.post("/", async (req, res, next) => {
+  try {
+    const logEntry = new LogEntry(req.body);
+    const createdEntry = await logEntry.save();
+    res.json(createdEntry);
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      res.status(422);
     }
-    // console.log(logEntry);
-    // console.log(req.body)
+    next(error);
+  }
+  // console.log(logEntry);
+  // console.log(req.body)
 });
 
 module.exports = router;
--- server/src/index.js
+++ server/src/index.js
@@ -1,40 +1,43 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const middlewares = require('./middlewares');
-const logs = require('./api/logs');
+const middlewares = require("./middlewares");
+const logs = require("./api/logs");
 
 const app = express();
 
 mongoose.connect(process.env.DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-app.use(morgan('common'));
+app.use(morgan("common"));
 app.use(helmet());
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN
+  })
+);
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello world!',
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello world!"
+  });
 });
 
-app.use('/api/logs', logs);
+app.use("/api/logs", logs);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
