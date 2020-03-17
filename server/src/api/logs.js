fbc3e8135faa37cbf2337e93ec128a27b569b983
yeounseo <yeoandante@gmail.com>
2020-03-17 18:37:35 +0900

    mapbox , react map gl connect and modified showLocation fuc

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -5,27 +5,25 @@ const LogEntry = require("../models/LogEntry");
 const router = Router();
 
 router.get("/", async (req, res, next) => {
-  try {
-    const entries = await LogEntry.find();
-    res.json(entries);
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const entries = await LogEntry.find();
+        res.json(entries);
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.post("/", async (req, res, next) => {
-  try {
-    const logEntry = new LogEntry(req.body);
-    const createdEntry = await logEntry.save();
-    res.json(createdEntry);
-  } catch (error) {
-    if (error.name === "ValidationError") {
-      res.status(422);
+    try {
+        const logEntry = new LogEntry(req.body);
+        const createdEntry = await logEntry.save();
+        res.json(createdEntry);
+    } catch (error) {
+        if (error.name === "ValidationError") {
+            res.status(422);
+        }
+        next(error);
     }
-    next(error);
-  }
-  // console.log(logEntry);
-  // console.log(req.body)
 });
 
 module.exports = router;
